ab54bdbc36d7c4242a3561eeac63b8bbe11da246
Michael Florya <mih2303@mail.ru>
2024-03-29 20:46:32 +0300

    full create register form

--- delphy/userauths/forms.py
+++ delphy/userauths/forms.py
@@ -4,6 +4,11 @@ from userauths.models import User
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
     class Meta:
         model = User
         fields = ['username', 'email']
--- delphy/userauths/views.py
+++ delphy/userauths/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 
 def register_view(request):
     if request.method == 'POST':
-        print('User registered')
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hello {username}, Your account war created successfuly')
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1']
+                                    )
+            login(request, new_user)
+            return redirect('core:index')
     else:
-        print('User cannot be registered')
-    form = UserRegisterForm()
+        form = UserRegisterForm()
 
     context = {
         'form': form,
