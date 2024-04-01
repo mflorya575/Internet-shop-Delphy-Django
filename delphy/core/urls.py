f611a50208f28edee70aa0fc2086b58ff7ff5fbe
Michael Florya <mih2303@mail.ru>
2024-04-01 23:01:04 +0300

    create category list (url, template & view)

--- delphy/core/urls.py
+++ delphy/core/urls.py
@@ -7,6 +7,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('products', views.product_list_view, name='product_list'),
-    # path('', views.index, name='index'),
+    path('products/', views.product_list_view, name='product_list'),
+    path('category/', views.category_list_view, name='category_list'),
 ]
--- delphy/core/views.py
+++ delphy/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+from django.db.models import Count
 
 
 def index(request):
@@ -21,3 +22,13 @@ def product_list_view(request):
     }
 
     return render(request, 'core/product-list.html', context)
+
+
+def category_list_view(request):
+    categories = Category.objects.all()  # categories = Category.objects.all().annotate(product_count=Count("product"))
+
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, 'core/category-list.html', context)
