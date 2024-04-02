da6c107c0f0cad78b26d8b8aa38001cba819d587
Michael Florya <mih2303@mail.ru>
2024-04-02 23:08:48 +0300

    create category product list view (view, url & template)

--- delphy/core/urls.py
+++ delphy/core/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.product_list_view, name='product_list'),
     path('category/', views.category_list_view, name='category_list'),
+    path('category/<cid>/', views.category_product_list_view, name='category_product_list'),
 ]
