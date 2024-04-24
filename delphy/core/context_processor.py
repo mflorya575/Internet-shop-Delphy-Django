31731e65af38493c61fa6d69e88d71a73a4062a1
Michael Florya <mih2303@mail.ru>
2024-04-24 16:47:46 +0300

    full create page product-detail (controller, layout)

--- delphy/core/context_processor.py
+++ delphy/core/context_processor.py
@@ -1,9 +1,17 @@
-from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+from core.models import Product, Category, Vendor, CartOrder,\
+    CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def default(request):
     categories = Category.objects.all()
+    try:
+        address = Address.objects.get(user=request.user)
+    except ObjectDoesNotExist:
+        address = None
 
     return {
         'categories': categories,
+        'address': address,
     }
